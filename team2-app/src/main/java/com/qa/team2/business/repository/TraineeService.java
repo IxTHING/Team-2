d5e4b44ce0ca0bfaeb02714c7b8eecb9fcedd7ba
Jordan White <jordan.white191191@yahoo.co.uk>
2018-01-20 17:53:48 +0000

    trainee Service

--- team2-app/src/main/java/com/qa/team2/business/repository/TraineeService.java
+++ team2-app/src/main/java/com/qa/team2/business/repository/TraineeService.java
@@ -1,5 +1,25 @@
 package com.qa.team2.business.repository;
 
-public interface TraineeService {
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
 
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public interface TraineeService {
+	
+	@WebMethod
+	String getAllTrainees();
+
+	@WebMethod
+	String addNewTrainee(String trainee);
+
+	@WebMethod
+	String deleteTrainee(Long id);
+	
+	@WebMethod
+	String findTrainee(Long id);
+	
+	@WebMethod
+	String updateTrainee(Long id, String trainee);
 }
