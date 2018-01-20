1cb1be3c892499a9ee8aced26fd82c4f74efb08f
Jordan White <jordan.white191191@yahoo.co.uk>
2018-01-20 17:55:07 +0000

    serviceImpl

--- team2-app/src/main/java/com/qa/team2/business/repository/TraineeServiceImpl.java
+++ team2-app/src/main/java/com/qa/team2/business/repository/TraineeServiceImpl.java
@@ -1,74 +1,36 @@
 package com.qa.team2.business.repository;
 
-import static javax.transaction.Transactional.TxType.REQUIRED;
-import static javax.transaction.Transactional.TxType.SUPPORTS;
+import javax.jws.WebService;
 
-import java.util.HashMap;
-import java.util.Map;
+@WebService(endpointInterface = "com.qa.team2.business.repository.TeamService") 
+public class TraineeServiceImpl implements TraineeService {
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Default;
-import javax.inject.Inject;
-import javax.transaction.Transactional;
-
-import com.qa.team2.persistence.Trainee;
-import com.qa.team2.util.XMLUtil;
-
-@Default
-@Transactional(SUPPORTS)
-@ApplicationScoped
-public class TraineeServiceImpl {
-
-	private Map<Long, Trainee> traineeMap;
-	private Long ID;
-	
-	@Inject
-	private XMLUtil util;
-	
-	public TraineeServiceImpl() {
-		this.traineeMap = new HashMap<Long, Trainee>();
-		ID = 0L;
-	}
-	
+	@Override
 	public String getAllTrainees() {
-		return util.getXMLForObject(traineeMap);
+		return "Hello Trainee";
 	}
-	
-	public String findTrainee(Long id) {
-		Trainee aTrainee = traineeMap.get(id);
-		return util.getXMLForObject(aTrainee);
+
+	@Override
+	public String addNewTrainee(String bookJson) {
+		// TODO Auto-generated method stub
+		return null;
 	}
-	
-	@Transactional(REQUIRED)
-	public String createTrainee(String traineeXML) {
-		Trainee newTrainee = (Trainee) util.getObjectForXML(traineeXML);
-		if (newTrainee != null) {
-			ID++;
-			traineeMap.put(ID, newTrainee);			
-			return "{\"message\": \"trainee successfully added\"}";
-		}
-		return "{\"message\": \"trainee not created - trainee to create was null\"}";
-	}
-	
-	@Transactional(REQUIRED)
-	public String updateTrainee(Long id, String traineeXML) {
-		Trainee aTrainee = traineeMap.get(id);
-		Trainee newTrainee = (Trainee) util.getObjectForXML(traineeXML);
-		if (aTrainee != null) {
-			traineeMap.put(id, newTrainee);
-			return "{\"message\": \"trainee successfully updated\"}";
-		}
-		return "{\"message\": \"trainee not updated - trainee at that id is null\"}";
-	}
-	
-	@Transactional(REQUIRED)
+
+	@Override
 	public String deleteTrainee(Long id) {
-		Trainee aTrainee = traineeMap.get(id);
-		if (aTrainee != null) {
-			traineeMap.remove(id);
-			return "{\"message\": \"trainee successfully deleted\"}";
-		}
-		return "{\"message\": \"trainee not deleted - trainee to delete is null\"}";
+		// TODO Auto-generated method stub
+		return null;
 	}
-	
+
+	@Override
+	public String findTrainee(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateTrainee(Long id, String apartment) {
+		// TODO Auto-generated method stub
+		return null;
+	}	
 }
